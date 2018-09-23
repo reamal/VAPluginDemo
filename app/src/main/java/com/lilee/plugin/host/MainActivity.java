d489b9a342b903987044e3b27eb9bffa1fb002e1
Lilee <lili@wkzf.com>
2018-09-23 11:29:01 +0800

    增加 content provider 使用方法。

--- app/src/main/java/com/lilee/plugin/host/MainActivity.java
+++ app/src/main/java/com/lilee/plugin/host/MainActivity.java
@@ -2,9 +2,11 @@ package com.lilee.plugin.host;
 
 import android.app.Service;
 import android.content.ComponentName;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.net.Uri;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +15,8 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.didi.virtualapk.PluginManager;
+import com.didi.virtualapk.internal.LoadedPlugin;
+import com.didi.virtualapk.internal.PluginContentResolver;
 import com.lilee.plugin.lib.IMyInterface;
 
 import java.io.File;
@@ -20,7 +24,8 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "liTag";
-    String apkName = "firstplugin.apk";
+    String apkName = "firstplugin-beijing-release.apk";
+    // demo ContentProvider 的URI
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -47,48 +52,48 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "plugin apk not exists !!!");
         }
 
-        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClassName(MainActivity.this, "com.lilee.plugin.first.MainActivity");
-                startActivity(intent);
-            }
-        });
+        findViewById(R.id.btn).setOnClickListener(v -> startActivity());
+        findViewById(R.id.btn_startService).setOnClickListener(v -> startService());
+        findViewById(R.id.btn_bindService).setOnClickListener(v -> bindService());
+        findViewById(R.id.btn_unbindService).setOnClickListener(v -> unbindService());
+        findViewById(R.id.btn_stopService).setOnClickListener(v -> stopService());
+        findViewById(R.id.btn_cpInsert).setOnClickListener(v -> cpInsert());
 
-
-        findViewById(R.id.btn_startService).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClassName(MainActivity.this, "com.lilee.plugin.first.PluginService");
-                startService(intent);
-            }
-        });
-
-
-        findViewById(R.id.btn_bindService).setOnClickListener(v ->
-                bindService(v)
-        );
-        findViewById(R.id.btn_unbindService).setOnClickListener(v ->
-                unbindService(v)
-        );
-        findViewById(R.id.btn_stopService).setOnClickListener(v ->
-                stopService(v)
-        );
     }
 
-    public void bindService(View view) {
+    private void cpInsert() {
+
+        String pkg = "com.lilee.plugin.first";
+        LoadedPlugin plugin = PluginManager.getInstance(this).getLoadedPlugin(pkg);
+        Uri cpUri = Uri.parse("content://com.lilee.plugin.first.Lilee");
+        cpUri = PluginContentResolver.wrapperUri(plugin, cpUri);
+        Integer count = getContentResolver().delete(cpUri, "where", null);
+        Toast.makeText(MainActivity.this, String.valueOf(count), Toast.LENGTH_LONG).show();
+    }
+
+    private void startActivity() {
+        Intent intent = new Intent();
+        intent.setClassName(MainActivity.this, "com.lilee.plugin.first.MainActivity");
+        startActivity(intent);
+    }
+
+    private void startService() {
+        Intent intent = new Intent();
+        intent.setClassName(MainActivity.this, "com.lilee.plugin.first.PluginService");
+        startService(intent);
+    }
+
+    public void bindService() {
         Intent intent = new Intent();
         intent.setClassName(MainActivity.this, "com.lilee.plugin.first.PluginService");
         bindService(intent, conn, Service.BIND_AUTO_CREATE);
     }
 
-    public void unbindService(View view) {
+    public void unbindService() {
         unbindService(conn);
     }
 
-    public void stopService(View view) {
+    public void stopService() {
         Intent intent = new Intent();
         intent.setClassName(MainActivity.this, "com.lilee.plugin.first.PluginService");
         stopService(intent);
---
+++ firstplugin/src/main/java/com/lilee/plugin/first/CProvider.java
@@ -0,0 +1,266 @@
+package com.lilee.plugin.first;
+
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.CharArrayBuffer;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.database.DataSetObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class CProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        System.out.println("===onCreate===");
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        System.out.println(uri + "===query===");
+        System.out.println("selection:" + selection);
+        return new Cursor() {
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public int getPosition() {
+                return 0;
+            }
+
+            @Override
+            public boolean move(int offset) {
+                return false;
+            }
+
+            @Override
+            public boolean moveToPosition(int position) {
+                return false;
+            }
+
+            @Override
+            public boolean moveToFirst() {
+                return false;
+            }
+
+            @Override
+            public boolean moveToLast() {
+                return false;
+            }
+
+            @Override
+            public boolean moveToNext() {
+                return false;
+            }
+
+            @Override
+            public boolean moveToPrevious() {
+                return false;
+            }
+
+            @Override
+            public boolean isFirst() {
+                return false;
+            }
+
+            @Override
+            public boolean isLast() {
+                return false;
+            }
+
+            @Override
+            public boolean isBeforeFirst() {
+                return false;
+            }
+
+            @Override
+            public boolean isAfterLast() {
+                return false;
+            }
+
+            @Override
+            public int getColumnIndex(String columnName) {
+                return 0;
+            }
+
+            @Override
+            public int getColumnIndexOrThrow(String columnName) throws IllegalArgumentException {
+                return 0;
+            }
+
+            @Override
+            public String getColumnName(int columnIndex) {
+                return null;
+            }
+
+            @Override
+            public String[] getColumnNames() {
+                return new String[0];
+            }
+
+            @Override
+            public int getColumnCount() {
+                return 0;
+            }
+
+            @Override
+            public byte[] getBlob(int columnIndex) {
+                return new byte[0];
+            }
+
+            @Override
+            public String getString(int columnIndex) {
+                return null;
+            }
+
+            @Override
+            public void copyStringToBuffer(int columnIndex, CharArrayBuffer buffer) {
+
+            }
+
+            @Override
+            public short getShort(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public int getInt(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public long getLong(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public float getFloat(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public double getDouble(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public int getType(int columnIndex) {
+                return 0;
+            }
+
+            @Override
+            public boolean isNull(int columnIndex) {
+                return false;
+            }
+
+            @Override
+            public void deactivate() {
+
+            }
+
+            @Override
+            public boolean requery() {
+                return false;
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public boolean isClosed() {
+                return false;
+            }
+
+            @Override
+            public void registerContentObserver(ContentObserver observer) {
+
+            }
+
+            @Override
+            public void unregisterContentObserver(ContentObserver observer) {
+
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public void setNotificationUri(ContentResolver cr, Uri uri) {
+
+            }
+
+            @Override
+            public Uri getNotificationUri() {
+                return null;
+            }
+
+            @Override
+            public boolean getWantsAllOnMoveCalls() {
+                return false;
+            }
+
+            @Override
+            public void setExtras(Bundle extras) {
+
+            }
+
+            @Override
+            public Bundle getExtras() {
+                return null;
+            }
+
+            @Override
+            public Bundle respond(Bundle extras) {
+                return null;
+            }
+        };
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        System.out.println("~~getType~~ Uri : " + uri.toString());
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values)
+    {
+        System.out.println(uri + "===insert===");
+        System.out.println("values:" + values);
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String where, String[] whereArgs)
+    {
+        System.out.println(uri + "===delete===");
+        System.out.println("where:" + where);
+        return 222;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String where,
+                      String[] whereArgs)
+    {
+        System.out.println(uri + "===update===");
+        System.out.println("where:" + where + ",values:" + values);
+        return 2;
+    }
+}
