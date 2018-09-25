311b0440662be4d9ce91eaf5f99b1c2c4356a477
Lilee <lili@wkzf.com>
2018-09-25 10:51:16 +0800

    增加申请读取sd卡权限功能。

--- app/src/main/java/com/lilee/plugin/host/MainActivity.java
+++ app/src/main/java/com/lilee/plugin/host/MainActivity.java
@@ -1,13 +1,21 @@
 package com.lilee.plugin.host;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.Service;
 import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
+import android.os.Process;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,6 +31,7 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_CODE_STORAGE = 2018;
     private static final String TAG = "liTag";
     String apkName = "firstplugin-beijing-release.apk";
 
@@ -38,6 +47,24 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        if (hasPermission()) {
+            Log.d(TAG, "loadPlugin");
+            loadPlugin();
+        } else {
+            requestPermission();
+        }
+
+        findViewById(R.id.btn).setOnClickListener(v -> startActivity());
+        findViewById(R.id.btn_startService).setOnClickListener(v -> startService());
+        findViewById(R.id.btn_bindService).setOnClickListener(v -> bindService());
+        findViewById(R.id.btn_unbindService).setOnClickListener(v -> unbindService());
+        findViewById(R.id.btn_stopService).setOnClickListener(v -> stopService());
+        findViewById(R.id.btn_cpInsert).setOnClickListener(v -> cpInsert());
+        findViewById(R.id.btn_sendBroadcastReceiver).setOnClickListener(v -> sendBroadcastReceiver());
+
+    }
+
+    private void loadPlugin() {
         File apk = getFileStreamPath(apkName);
 
         if (apk.exists()) {
@@ -51,15 +78,71 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "plugin apk not exists !!!", Toast.LENGTH_SHORT).show();
             Log.e(TAG, "plugin apk not exists !!!");
         }
+    }
 
-        findViewById(R.id.btn).setOnClickListener(v -> startActivity());
-        findViewById(R.id.btn_startService).setOnClickListener(v -> startService());
-        findViewById(R.id.btn_bindService).setOnClickListener(v -> bindService());
-        findViewById(R.id.btn_unbindService).setOnClickListener(v -> unbindService());
-        findViewById(R.id.btn_stopService).setOnClickListener(v -> stopService());
-        findViewById(R.id.btn_cpInsert).setOnClickListener(v -> cpInsert());
-        findViewById(R.id.btn_sendBroadcastReceiver).setOnClickListener(v -> sendBroadcastReceiver());
+    private boolean hasPermission() {
+        Log.d(TAG, "hasPermission");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        }
+        return true;
+    }
 
+
+    private void requestPermission() {
+
+        Log.d(TAG, "requestPermission");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            boolean b = shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (b) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE_STORAGE);
+            } else {
+                Toast.makeText(this, "需要读取SD卡权限", Toast.LENGTH_SHORT).show();
+                AlertDialog alertDialog = new AlertDialog.Builder(this)
+                        .setTitle("权限申请提示：")
+                        .setMessage("需要读取SD卡权限，前往设置打开")
+                        .setPositiveButton("确定", (dialog, which) -> goSettingPage())
+                        .setNegativeButton("退出", (dialog, which) -> Process.killProcess(Process.myPid())).create();
+                alertDialog.show();
+            }
+        }
+    }
+
+    private void goSettingPage() {
+        Context context = MainActivity.this.getApplicationContext();
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", context.getPackageName(), null);
+        intent.setData(uri);
+        MainActivity.this.startActivityForResult(intent, PERMISSION_REQUEST_CODE_STORAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE_STORAGE:
+                if (hasPermission()) {
+                    Log.d(TAG, "loadPlugin");
+                    loadPlugin();
+                } else {
+                    requestPermission();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (PERMISSION_REQUEST_CODE_STORAGE == requestCode) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                requestPermission();
+            } else {
+                loadPlugin();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     private void sendBroadcastReceiver() {
